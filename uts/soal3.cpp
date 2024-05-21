2133d320fe48e653ebf73903147275bd548d2bb0
Anraaa <aqlaharun03@gmail.com>
2024-05-21 10:08:03 +0700

    fresh from the oven

--- uts/soal3.cpp
+++ uts/soal3.cpp
@@ -8,10 +8,10 @@ int main()
     cout << "Masukkan nilai n: ";
     cin >> n;
     for (int i = 1; i<=n; i++ ){
-        for (int s = 1; s<=n-i; s++){
+        for (int s = 1; s<n; s++){
             cout << " ";
         }
-        for (int j = 1; j<=2*i-1; j++){
+        for (int j = 1; j<i; j++){
             cout << "*";
         }
         cout << "\n";
