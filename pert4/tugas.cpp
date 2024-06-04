60037575a16cc77396fa2e654c47a32e1a7bd415
Anraaa <aqlaharun03@gmail.com>
2024-06-04 09:06:38 +0700

    fresh from the oven

--- pert4/tugas.cpp
+++ pert4/tugas.cpp
@@ -2,45 +2,69 @@
 
 using namespace std;
 
+void tambah(int a, int b){
+    cout << "TAMBAH : " << a + b <<endl;
+}
+void kurang(int a, int b){
+    cout << "KURANG" << a - b <<endl;
+}
+
+void kali(int a, int b){
+    cout << "KALI" << a * b <<endl;
+}
+
+void bagi(int a, int b){
+    cout << "BAGI" << a / b <<endl;
+}
+
 int main(){
-    int pilihan;
-    double angka1, angka2;
+    int a, b;
+    char pilih;
+    do{
+        cout << "MENU" << endl;
+        cout << "1. Tambah" << endl;
+        cout << "2. Kurang" << endl;
+        cout << "3. Kali" << endl;
+        cout << "4. Bagi" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Pilihan : ";
+        cin >> pilih;
 
-    cout << "masukkan angka pertama : " ;
-    cin >> angka1;
-
-    cout << "masukkan angka kedua : " ;
-    cin >> angka2;
-
-    cout << endl;
-    cout << "MENU" << endl;
-    cout << "1. Pertambah " << endl;
-    cout << "2. Kurang " << endl;
-    cout << "3. Kali " << endl;
-    cout << "4. bagi " << endl;
-    cout << "Masukkan Pilihan " ;
-    cin >> pilihan;
-
-    switch(pilihan){
-        case 1 :
-            cout << "Hasil : " << angka1 + angka2 << endl;
-            break;
-        case 2 :
-            cout << "Hasil : " << angka1 - angka2 << endl;
-            break;
-        case 3 :
-            cout << "Hasil : " << angka1 * angka2 << endl;
-            break;
-        case 4 :
-            if(angka2 != 0) {
-                cout << "Hasil : " << angka1 / angka2 << endl;
-            } else {
-                cout << "Error: Division by zero is not allowed." << endl;
-            }
-            break;
-        default:
-            cout << "Invalid option selected." << endl;
-            break;
-    }
+        switch (pilih){
+            case '1':
+                cout << "Masukkan Angka 1 : ";
+                cin >> a;
+                cout << "Masukkan Angka 2 : ";
+                cin >> b;
+                tambah(a, b);
+                break;
+            case '2':
+                cout << "Masukkan Angka 1 : ";
+                cin >> a;
+                cout << "Masukkan Angka 2 : ";
+                cin >> b;
+                kurang(a, b);
+                break;
+            case '3':
+                cout << "Masukkan Angka 1 : ";
+                cin >> a;
+                cout << "Masukkan Angka 2 : ";
+                cin >> b;
+                kali(a, b);
+                break;
+            case '4':
+                cout << "Masukkan Angka 1 : ";
+                cin >> a;
+                cout << "Masukkan Angka 2 : ";
+                cin >> b;
+                bagi(a, b);
+                break;
+            case '5':
+                cout << "EXIT" << endl;
+                break;
+            default:
+                cout << "Pilihan Salah!" << endl;
+        }
+    }while(pilih != '5');
     return 0;
 }
